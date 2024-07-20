a54defa09efe2a8e72cad6304f3767ca52713f7d
CocoByte <nicolle.leclair@gmail.com>
2024-07-19 18:19:08 -0600

    “update”

--- src/registrar/management/commands/drop_tables.py
+++ src/registrar/management/commands/drop_tables.py
@@ -2,7 +2,7 @@ import logging
 from django.conf import settings
 from django.core.management import BaseCommand
 from django.apps import apps
-from django.db import transaction
+from django.db import connection, transaction
 
 from registrar.management.commands.utility.terminal_helper import TerminalHelper
 
@@ -19,8 +19,21 @@ class Command(BaseCommand):
             logger.error("clean_tables cannot be run in production")
             return
 
-        self.stdout.write(self.style.WARNING('Dropping all tables...'))
+        self.print_tables()
+        logger.info(self.style.WARNING('Dropping all tables...'))
         with connection.cursor() as cursor:
             cursor.execute("DROP SCHEMA public CASCADE;")
             cursor.execute("CREATE SCHEMA public;")
-        self.stdout.write(self.style.SUCCESS('All tables dropped.'))
+        logger.info(self.style.SUCCESS('All tables dropped.'))
+
+    def print_tables(self):
+        self.stdout.write(self.style.WARNING('Fetching table names...'))
+        with connection.cursor() as cursor:
+            cursor.execute("SELECT table_name FROM information_schema.tables WHERE table_schema='public'")
+            table_names = cursor.fetchall()
+            if table_names:
+                self.stdout.write(self.style.NOTICE('Tables in the database:'))
+                for name in table_names:
+                    self.stdout.write(name[0])
+            else:
+                self.stdout.write('No tables found.')
